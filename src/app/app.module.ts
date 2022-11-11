3760057ebb2c2256867b45f344a9e5b36a1a2b75
Gustavo Guntzel <gustavo.g.s01@hotmail.com>
2022-11-11 15:27:35 -0300

    Static files done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
+import { RodapeComponent } from './components/rodape/rodape.component';
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent],
+  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css'],
+})
+export class CriarPensamentoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  criaPensamento() {
+    alert('Pensamento criado');
+  }
+
+  cancelaPensamento() {
+    alert('Pensamento cancelado');
+  }
+}
